d72da9d9d0fc842f7e77a034d7df25ce0658a270
Jake Samuelson <samuelsonjakejk@gmail.com>
2017-12-01 17:11:51 -0500

    quiz challenge

--- COP3503 Project Group/COP3503 Project Group/quiz_challenge.hpp
+++ COP3503 Project Group/COP3503 Project Group/quiz_challenge.hpp
@@ -1,6 +1,17 @@
-//  quiz_challenge.hpp
-//  COP3503 Project Group
-//
-//  Created by Chad Wishner on 11/5/17.
-//  Copyright © 2017 Group 35. All rights reserved.
-//
+#define quiz_challenge_hpp
+#include "challenge.hpp"
+#include <stdio.h>
+
+using namespace std;
+
+class quizchallenge : public challenge{
+private:
+    int room;
+    int trivia;
+public:
+    quizchallenge(int room){
+        this -> room = room;
+    }
+    void go();
+};
+
