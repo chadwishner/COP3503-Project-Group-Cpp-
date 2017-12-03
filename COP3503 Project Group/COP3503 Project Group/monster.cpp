b7bc50900e39cc525d143996f933f0c04af62575
chadwishner <chadwishner@gmail.com>
2017-12-03 13:31:41 -0500

    constructor fix

--- COP3503 Project Group/COP3503 Project Group/monster.cpp
+++ COP3503 Project Group/COP3503 Project Group/monster.cpp
@@ -5,7 +5,7 @@
 
 #include "monster.hpp"
 
-Monster::Monster(int h, int mh, int s, int d, int a, int e, std::string n) : hp(h), maxhp(mh), str(s), def(d), sta(a), exp(e), name(n){}
+Monster::Monster(int h, int mh, int s, int d, int a, int e, std::string n) : Entity(h, mh, s, d, a, e, n), hp(h), maxhp(mh), str(s), def(d), sta(a), exp(e), name(n){}
 
 int Monster::die(){
     std::cout << "Congratulations! You have slain " << name << "!\n";
