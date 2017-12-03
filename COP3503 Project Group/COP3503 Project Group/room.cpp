5c1142b960b821d8e310ac2aee1ff823b418498a
Andrew So <so.andrew0@gmail.com>
2017-12-03 18:06:35 -0500

    Documenting main and room

--- COP3503 Project Group/COP3503 Project Group/room.cpp
+++ COP3503 Project Group/COP3503 Project Group/room.cpp
@@ -4,7 +4,8 @@
 //  Copyright © 2017 Group 35. All rights reserved.
 
 #include "room.hpp"
-#include "challenge.hpp"
+
+// Constructor
 
 Room::Room(challenge *c, Monster *m, std::string message, std::string flavorText, std::vector<std::string> choices){
     this->ch = c;
@@ -15,30 +16,30 @@ Room::Room(challenge *c, Monster *m, std::string message, std::string flavorText
     this->complete = false;
 }
 
-std::string Room::getMessage(){
+std::string Room::getMessage(){ // Returns the message to be displayed upon entering room
     return message;
 }
 
-std::vector<std::string> Room::getChoices(){
+std::vector<std::string> Room::getChoices(){ // Returns choices provided to the player as a vector
     return choices;
 }
 
-std::string Room::getFlavorText(){
+std::string Room::getFlavorText(){ // Returns flavor text to be displayed when "look around" choice is selected
     return flavorText;
 }
 
-Monster * Room::getMonster(){
+Monster * Room::getMonster(){ // Returns Monster associated with room
     return monster;
 }
 
-challenge * Room::getChallenge(){
+challenge * Room::getChallenge(){ // Returns challenge associated with room
     return ch;
 }
 
-void Room::setComplete(){
+void Room::setComplete(){ // Set flag "complete" to true
     this->complete = true;
 }
 
-bool Room::isComplete(){
+bool Room::isComplete(){ // Returns value of "complete"
     return complete;
 }
--- COP3503 Project Group/COP3503 Project Group/room.hpp
+++ COP3503 Project Group/COP3503 Project Group/room.hpp
@@ -13,21 +13,21 @@
 #include "monster.hpp"
 
 class Room{
-private:        challenge * ch;
-                Monster * monster;
+private:        challenge * ch; // Challenge associated with room
+                Monster * monster; // Monster associated with room
                 bool complete; // If true, player can move on to next room
                 std::string message; // Displayed when room is entered
                 std::string flavorText; // Displayed when "look around" is chosen
                 std::vector<std::string> choices; // Choices to be displayed
 
-public:         Room(challenge *c, Monster *m, std::string message, std::string flavorText, std::vector<std::string> choices);
-                std::string getMessage();
-                std::string getFlavorText();
-                std::vector<std::string> getChoices();
-                challenge * getChallenge();
-                Monster * getMonster();
-                void setComplete();
-                bool isComplete();
+public:         Room(challenge *c, Monster *m, std::string message, std::string flavorText, std::vector<std::string> choices); // Constructor
+                std::string getMessage(); // Message getter
+                std::string getFlavorText(); // Flavor text getter
+                std::vector<std::string> getChoices(); // Choices getter
+                challenge * getChallenge(); // Challenge getter
+                Monster * getMonster(); // Monster getter
+                void setComplete(); // Completion flag setter
+                bool isComplete(); // Completion flag getter
 };
 
 #endif /* room_hpp */
